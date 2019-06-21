88f568b662b8e22c0eb6c769477e00b178ab5a5c
MarianoDM22 <32902651+MarianoDM22@users.noreply.github.com>
2019-06-21 00:30:57 -0300

    commit

---
+++ TPRedesCliente/TPRedesCliente/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace TPRedesCliente
+{
+    class Program
+    {
+        private static readonly Socket ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+		static void Main()
+        {
+            Console.Title = "Cliente";
+            ConnectToServer();
+            RequestLoop();
+            Exit();
+        }
+
+        private static void ConnectToServer()
+        {
+            int attempts = 0;
+
+            while (!ClientSocket.Connected)
+            {
+                attempts++;
+				if(attempts > 1)
+				{
+					Console.WriteLine("Datos incorrectos");
+				}
+                Console.WriteLine("Intentando conexion, intento numero: " + attempts);
+				DataConnect();
+            }
+
+            Console.Clear();
+            Console.WriteLine("Conectado");
+        }
+
+		private static void DataConnect()
+		{
+			Console.WriteLine("Ingrese la IP del servidor: ");
+			string requestIp = Console.ReadLine();
+
+			Console.WriteLine("Ingrese el PUERTO del servidor: ");
+			int requestPort = Int32.Parse(Console.ReadLine());
+
+			try
+			{
+				ClientSocket.Connect(requestIp, requestPort);
+			}
+			catch(SocketException)
+			{
+				Console.Clear();
+			}
+		}
+
+        private static void RequestLoop()
+        {
+            Console.WriteLine(@"<Escribe ""x"" para desconectarte>");
+
+            while (true)
+            {
+                SendRequest();
+                ReceiveResponse();
+            }
+        }
+
+        private static void Exit()
+        {
+            SendString("x");
+            ClientSocket.Shutdown(SocketShutdown.Both);
+            ClientSocket.Close();
+            Environment.Exit(0);
+        }
+
+        private static void SendRequest()
+        {
+            Console.Write("Escriba su mensaje: ");
+            string request = Console.ReadLine();
+            SendString(request);
+
+            if (request.ToLower() == "x")
+            {
+                Exit();
+            }
+        }
+
+        private static void SendString(string text)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(text);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private static void ReceiveResponse()
+        {
+			var buffer = new byte[2048];
+			int received = ClientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0) return;
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+            Console.WriteLine("Mensaje del servidor: " + text);
+        }
+    }
+}
---
+++ TPRedesServidor/TPRedesServidor/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TPRedesServidor
+{
+	class Program
+	{
+		private static readonly Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		private static readonly List<Socket> clientSockets = new List<Socket>();
+		private const int BUFFER_SIZE = 2048;
+		private static readonly byte[] buffer = new byte[BUFFER_SIZE];
+		private const int PORT = 3000;
+		private const string IPLOCAL = "127.0.0.1";
+		
+		static void Main()
+		{
+			Console.Title = "Servidor";
+			SetupServer();
+
+			while(true)
+			{
+
+			}
+		}
+
+		private static void SetupServer()
+		{
+			Console.WriteLine("Iniciando servidor...");
+			serverSocket.Bind(new IPEndPoint(IPAddress.Parse(IPLOCAL), PORT));
+			serverSocket.Listen(0);
+			serverSocket.BeginAccept(AcceptCallback, null);
+			Console.WriteLine("Servidor listo");
+			Console.WriteLine("Esperando clientes...");
+		}
+
+		private static void CloseAllSockets()
+		{
+			foreach (Socket socket in clientSockets)
+			{
+				socket.Shutdown(SocketShutdown.Both);
+				socket.Close();
+			}
+
+			serverSocket.Close();
+		}
+
+		private static void AcceptCallback(IAsyncResult AR)
+		{
+			Socket socket;
+
+			try
+			{
+				socket = serverSocket.EndAccept(AR);
+			}
+			catch (ObjectDisposedException)
+			{
+				return;
+			}
+
+			clientSockets.Add(socket);
+			socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, socket);
+			Console.WriteLine("Cliente conectado...");
+			serverSocket.BeginAccept(AcceptCallback, null);
+		}
+
+		private static void ReceiveCallback(IAsyncResult AR)
+		{
+			Socket current = (Socket)AR.AsyncState;
+			int received;
+
+				try
+				{
+					received = current.EndReceive(AR);
+				}
+				catch (SocketException)
+				{
+					Console.WriteLine("El cliente se desconecto de forma forzosa");
+					current.Close();
+					clientSockets.Remove(current);
+					return;
+				}
+
+				byte[] recBuf = new byte[received];
+				Array.Copy(buffer, recBuf, received);
+				string text = Encoding.ASCII.GetString(recBuf);
+				Console.WriteLine("Mensaje del cliente: " + text);
+
+			if (text.ToLower() == "get time")
+			{
+				Console.WriteLine("El mensaje es un requerimiento de hora");
+				byte[] dataTime = Encoding.ASCII.GetBytes(DateTime.Now.ToLongTimeString());
+				current.Send(dataTime);
+				Console.WriteLine("La hora se envio al cliente");
+			}
+			else if (text.ToLower() == "x")
+			{
+				current.Shutdown(SocketShutdown.Both);
+				current.Close();
+				clientSockets.Remove(current);
+				Console.WriteLine("Cliente desconectado");
+				return;
+			}
+			else
+			{
+				Console.Write("Escriba su respuesta: ");
+				string response = Console.ReadLine();
+				byte[] data = Encoding.ASCII.GetBytes(response);
+				current.Send(data);
+
+				if (response.ToLower() == "x")
+				{
+					CloseAllSockets();
+				}
+			}
+
+			current.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ReceiveCallback, current);
+		}
+	}
+}
